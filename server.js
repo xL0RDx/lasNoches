d5c61525de7a5dce36cba669fd9c72d18ccb0c75
unknown <espinor9@gmatc.matc.edu>
2020-09-23 23:55:48 -0500

    Eigth commit

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import express from 'express';
+import connectDatabase from './config/db';
 
 // Initialize express application 
 const app = express();
 
+connectDatabase();
+
 // API endpoints
 app.get('/', (req,res) =>
-    res.send('http got request sent to root api endpoint')
+    res.send('http get request sent to root api endpoint')
 );
 
 // Connection listener
