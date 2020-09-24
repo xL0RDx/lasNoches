95fcfd33ad24bd35aa0d53e4d092d26e588863cb
unknown <espinor9@gmatc.matc.edu>
2020-09-23 23:02:20 -0500

    Fourth commit

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+// Initialize express application 
+const app = express();
+
+// API endpoints
+app.get('/', (req,res) =>
+    res.send('http get request sent to root api endpoint')
+);
+
+// Connection listener
+app.listen(3000, () => console.log('Express server running on port 3000'));
