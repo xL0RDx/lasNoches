a2685707817f8d3b75abeb92513cb4fac5eaa7b9
unknown <espinor9@gmatc.matc.edu>
2020-09-23 23:06:06 -0500

    Fifth commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 // API endpoints
 app.get('/', (req,res) =>
-    res.send('http get request sent to root api endpoint')
+    res.send('http got request sent to root api endpoint')
 );
 
 // Connection listener
