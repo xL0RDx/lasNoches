3f80ef76624cf81ba6c742e907245d683785c5b0
unknown <espinor9@gmatc.matc.edu>
2020-10-01 19:43:51 -0500

    more

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectDatabase from './config/db';
+import { check, validationResult } from 'express-validator';
 
 //initialize express application
 const app = express();
@@ -23,10 +24,24 @@ app.get('/', (req, res) =>
  * @route POST api/users
  * @desc Register user
  */
-app.post('/api/users', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
-    });
+app.post(
+    '/api/users',
+    [
+        check( 'name', 'Please enter your name').not().isEmpty(),
+        check('email', 'Please enter a valid email').isEmail(),
+        check('password', 'Please enter a password with 6 or more characters').isLength({ min : 6})
+    ],
+
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+
+        } else {
+            return res.send(req.body);
+        }     
+    }
+);
 
 //connection listener
 app.listen(3000,() => console.log('Express server running on port 3000'));
