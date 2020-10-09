78977558f6b9a92c66d2303f7a41b2386a9220f0
unknown <espinor9@gmatc.matc.edu>
2020-10-08 21:33:58 -0500

    maybe

---
+++ client/build/precache-manifest.452eaccec6c5af8e29faf53abc6b286e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc93db72576f18708df695eca21c6430",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a1aeb0e34640cba567a",
+    "url": "/static/css/main.f45226ac.chunk.css"
+  },
+  {
+    "revision": "d634b92862562113eb0e",
+    "url": "/static/js/2.fe640a42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fe640a42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a1aeb0e34640cba567a",
+    "url": "/static/js/main.07d4a267.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
